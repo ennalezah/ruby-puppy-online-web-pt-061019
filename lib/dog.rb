3f70bf3d4338696ecb10d511698b43894d958b12
Hazel Anne Villareal <hae.villareal@gmail.com>
2019-07-05 22:46:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,23 @@
 # Add your code here
+
+class Dog 
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    self.all.each {|name| puts "#{@name}"}
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+end
